f26f456fd07850829ab8d523a19fff78cf5857b9
nothing17777 <yuqian1786392229@gmail.com>
2025-11-07 18:31:18 -0800

    Added Transitions on buttons and removed index.css

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,36 +1,34 @@
 import React from "react";
 
 function Home(props) {
-  const user = props.user
-  if (user){
+  const user = props.user;
+  if (user) {
     return (
-      <div style={{ textAlign: "center" }}>
-        <h2>🐻 UCLA Delivery NetWork 🐻</h2>
+      <div className="home-container">
+        <h2>🐻 UCLA Delivery Network 🐻</h2>
         <p>Welcome back, {user.name}</p>
         <p>Post a delivery request, or pick up a delivery request?</p>
-        
+
         <a href="/requests/new">
-          <button>Create Requests</button>
+          <button className="home-btn">Create Requests</button>
         </a>
 
         <a href="/requests">
-          <button>View Requests</button>
+          <button className="home-btn">View Requests</button>
         </a>
       </div>
     );
-  }
-  else{
-    return(
-      <div style={{ textAlign: "center" }}>
-        <h2>🐻 UCLA Delivery NetWork 🐻</h2>
-        <p>
-          Please login or signup to starting sending or accepting delivery request!
-        </p>
+  } else {
+    return (
+      <div className="home-container">
+        <h2>🐻 UCLA Delivery Network 🐻</h2>
+        <p>Please login or signup to start sending or accepting delivery requests!</p>
+
         <a href="/login">
-          <button>Login</button>
+          <button className="home-btn">Login</button>
         </a>
         <a href="/signup">
-          <button>Signup</button>
+          <button className="home-btn">Signup</button>
         </a>
       </div>
     );
