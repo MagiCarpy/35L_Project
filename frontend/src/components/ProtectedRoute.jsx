f11a9bb30885720db0071137778bab7b45aac5f8
Timothy Ha <timmyha9@gmail.com>
2025-11-12 21:44:40 -0800

    Refactor ProtectedRoute: simplified logic, used direct user prop,  remove doutdated context usage

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,9 +1,14 @@
 import { Navigate, Outlet } from "react-router-dom";
 import Loading from "../pages/Loading/Loading";
 
-function ProtectedRoute({ user, isLoading, redirect = "/" }) {
+function ProtectedRoute({ user, isLoading, redirect = "/login" }) {
   if (isLoading) return <Loading />;
-  return user ? <Outlet context={user} /> : <Navigate to={redirect} replace />;
+
+  //If no user, kick em out
+  if (!user) return <Navigate to={redirect} replace />;
+
+  //Otherwise, render nested route
+  return <Outlet />;
 }
 
 export default ProtectedRoute;
