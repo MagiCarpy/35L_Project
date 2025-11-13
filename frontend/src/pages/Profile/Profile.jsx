964c01a8e9e8a3c3717d31845409c7e9988954a1
Timothy Ha <timmyha9@gmail.com>
2025-11-12 21:42:19 -0800

    Refactor Profile.jsx: use user prop, remove redundant redirect

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -4,30 +4,27 @@ import { useOutletContext, Navigate } from "react-router-dom";
 // maybe make parent component that keeps track of user creds?
 // because don't want to have to rewrite get user logic
 
-function Profile() {
-  const user = useOutletContext();
+import React from "react";
 
+function Profile({ user }) {
   if (!user) {
-    // Redirect to login if user is not authenticated
-    return <Navigate to="/login" replace />;
+    return <p>No user data available.</p>;
   }
 
   return (
-    user && (
-      <>
-        <h2>Profile</h2>
-        <p>
-          <b>UserId:</b> {user.userId || "N/A"}
-        </p>
-        <p>
-          <b>Username:</b> {user.username || "N/A"}
-        </p>
-        <p>
-          <b>Email:</b> {user.email || "N/A"}
-        </p>
-      </>
-    )
+    <>
+      <h2>Profile</h2>
+      <p>
+        <b>UserId:</b> {user.userId || "N/A"}
+      </p>
+      <p>
+        <b>Username:</b> {user.username || "N/A"}
+      </p>
+      <p>
+        <b>Email:</b> {user.email || "N/A"}
+      </p>
+    </>
   );
 }
 
-export default Profile;
+export default Profile; 
