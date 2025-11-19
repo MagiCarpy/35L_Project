15693e1a63387f34ae9617db4560238633f21eb1
Timothy Ha <timmyha9@gmail.com>
2025-11-19 12:43:06 -0800

    More residence halls

--- frontend/src/constants/resHalls.js
+++ frontend/src/constants/resHalls.js
@@ -1,17 +1,84 @@
 export const RES_HALLS = {
+    
+  // Olympic / Centennial
   olympic: {
     label: "Olympic Hall",
-    lat: 34.07264871213392,
-    lng: -118.45342609619159,
+    lat: 34.072301297196205,
+    lng: -118.4533123533812,
   },
-  rieber: {
+  centennial: {
+    label: "Centennial Hall",
+    lat: 34.072865912889455,
+    lng: -118.4537106381686,
+  },
+
+  // Hedrick 
+  hedrick_hall: {
+    label: "Hedrick Hall",
+    lat: 34.073400553683705,
+    lng: -118.45249219386774,
+  },
+  hedrick_summit: {
+    label: "Hedrick Summit",
+    lat: 34.07386253445175,
+    lng: -118.45275530023869,
+  },
+
+  // Rieber
+  rieber_hall: {
     label: "Rieber Hall",
-    lat: 34.07171468015748,
-    lng: -118.45159131481672,
+    lat: 34.07186989748192,
+    lng: -118.45170326093951,
   },
-  sproul: {
+  rieber_terrace: {
+    label: "Rieber Terrace",
+    lat: 34.07250853868165,
+    lng: -118.45218413101477,
+  },
+  rieber_vista: {
+    label: "Rieber Vista",
+    lat: 34.07225641878643,
+    lng: -118.45207929329993,
+  },
+
+  // Sproul
+  sproul_cove: {
+    label: "Sproul Cove",
+    lat: 34.071664973824866,
+    lng: -118.4508403727209,
+  },
+  sproul_landing: {
+    label: "Sproul Landing",
+    lat: 34.07156141262478,
+    lng: -118.45026911428403,
+  },
+  sproul_hall: {
     label: "Sproul Hall",
-    lat: 34.07247865121819,
-    lng: -118.45039425401765,
-  }
+    lat: 34.072362831220644,
+    lng: -118.45031547490954,
+  },
+  delta_terrace: {
+    label: "Delta Terrace",
+    lat: 34.073216945175574,
+    lng: -118.45081200474037,
+  },
+  courtside: {
+    label: "Courtside Suites",
+    lat: 34.07354524815629,
+    lng: -118.4501543734435,
+  },
+
+  // De Neve- add more pls
+  dykstra: {
+    label: "Dykstra Hall",
+    lat: 34.070105679663214,
+    lng: -118.45011144771361,
+  },
+
+  // Random other locations (haha funny)
+  rolfe: {
+    label: "Rolfe",
+    lat: 34.07355765735547,
+    lng: -118.4419051707709,
+  },
 };
