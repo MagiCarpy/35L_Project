f0192b1e4aa94dc0b8eb57274a3447217b2c8398
Brandon-Kam <brandonjkam@gmail.com>
2025-12-03 02:31:59 -0800

    merge username search

--- backend/server.js
+++ backend/server.js
@@ -52,22 +52,6 @@ app.use("/api/requests", requestRoutes);
 app.use("/api/directions", directionsRoutes);
 app.use("/api/messages", messageRoutes);
 
-<<<<<<< HEAD
-=======
-
-// FIXME: delete this lol (test error page)
-app.get("/testError", async (req, res, next) => {
-  try {
-    await sleep(2000);
-    throw new Error("TEST");
-  } catch (error) {
-    console.log("error");
-    console.error(error.message);
-    next(error);
-  }
-});
-
->>>>>>> username-branch
 // Error Handling Middleware
 app.use((err, req, res, next) => {
   console.error("Error", err);
